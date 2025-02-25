0d17292650ce002e48a49b806fdc0b77455c6c6f
LepsyMikolaj3301 <miki2004200@wp.pl>
2025-02-25 17:46:30 +0100

    New changes and llll

--- src/app.py
+++ src/app.py
@@ -1,26 +1,28 @@
 from flask import Flask, render_template  
-from apscheduler.schedulers.background import BackgroundScheduler
+import schedule
 import os
-import shutil
 from datetime import datetime
 import requests
+from dotenv import load_dotenv
+import json
+
 
 # creating a Flask application 
+
 app = Flask(__name__) 
-
-# GLOBAL VARS
-IMAGE_DIR = "static/images"
-CURRENT_IMAGE = os.path.join(IMAGE_DIR, "current.jpg")
+load_dotenv()
 
 
-# SCHEDULE USING SCHEDULER
-# Schedule the actions
-scheduler = BackgroundScheduler()
+# GLOBAL STATIC VARS
 
-def get_posts(api_key):
+IMAGE_DIR = "static/cat_info/"
+API_KEY_CATS = os.getenv('API_KEY_CATS')
+ 
+
+def get_posts() -> list:
     # API KEY IS ENCRYPTED
-    limit = 24
-    endpoint_url = f'https://api.thecatapi.com/v1/images/search?limit={limit}&api_key={api_key}'
+    
+    endpoint_url = f'https://api.thecatapi.com/v1/images/search?limit={limit}&api_key={API_KEY_CATS}'
     try:
         # MAKE GET REQUEST TO THE API
         response = requests.get(endpoint_url)
@@ -35,61 +37,85 @@ def get_posts(api_key):
         # Handle any network-related errors or exceptions
         print('ERROR - OTHER:', e)
         return None
-    
+   
 
-# Get 24 random cat images at midnight (before a bit, not to confuse the algorithm below)
-@scheduler.scheduled_job('cron', hour=23, minute=57)
-def update_images_dir():
-    # all the images as paths (for use later, to delete)
-    all_old_images_paths = [os.path.join(IMAGE_DIR, img) for img in os.listdir(IMAGE_DIR)]
+class CurrentPhotos():
+    def __init__(self):
+        self._current_day_cats: list = json.load(IMAGE_DIR + 'current_day_cats.json')
+        self._cur_cat_index = 1
+        self._current_cat = self._current_day_cats[0]  
+        
+    def update_images_dir(self):
     
-    # get the json object from api
-    list_of_img_info = get_posts()
+        with open(IMAGE_DIR + 'current_day_cats.json', 'w') as file_current_cats, open(IMAGE_DIR + 'last_day_cats.json', 'w') as file_last_cats:
+            # loading in the info of last cats (last day)
+            cat_photos_to_be_changed = json.load(file_current_cats)
+            
+            try:
+                # get the json object from api (for today)
+                current_day_cats = get_posts()
+                
+                # saving the last cats to the last json
+                json.dump(cat_photos_to_be_changed, file_last_cats, indent=6)
+                
+                if current_day_cats:
+                    json.dump(current_day_cats, file_current_cats, indent=6)
+                    print(f'CATS SUCCESSFULLY UPDATED AT {datetime.now()}')
+                else:
+                    raise Exception("Error with API")
+            except Exception as e:
+                print(f'Something went wrong :( - {e}')
+                # if something gets wrong - stay with old photos
+                json.dump(cat_photos_to_be_changed, file_current_cats, indent=6)
+
+        # IN the end - update the index
+        self._cur_cat_index = 0
         
         
-    
-    
-    
-    pass
-
-
-# Updating the image
-
-@scheduler.scheduled_job('interval', hour=1)
-def update_cur_image(used_images) -> str:
-    """This functions is called to update the currently displayed image to a new one 
-    """
-    # get the current list of images ( not the current one and the used ones)
-    available_images = [img for img in os.listdir(IMAGE_DIR) 
-                                if img != "current.jpg" and img not in used_images]
-    
-    if available_images:
-        # get a new
-        new_image = available_images.pop()
+    def update_cur_image(self):
+        """This functions is called to update the currently displayed image to a new one 
+        """
+        # get the next cat info and image
+        with open(IMAGE_DIR + 'current_day_cats.json') as file_current_cats:
+            self._current_day_cats = json.load(file_current_cats)
+            
         
-        new_image_path = os.path.join(IMAGE_DIR, new_image)
-        
-        # update current image
-        shutil.copy(new_image_path, CURRENT_IMAGE)  # Replace current.jpg with the new image
-        
-        # log print
-        print(f"[ {datetime.now()} ] Image updated to: {new_image}")
+        if self._current_day_cats:
+            
+            # UPDATE CURRENT CAT
+            self._current_cat = self._current_day_cats[self._cur_cat_index]
+            
+            # log print
+            print(f"[ {datetime.now()} ] Image updated to (cat ID): {self._current_cat['id']}")
+            
+            # update the index
+            self._cur_cat_index += 1
+            
+    def get_curr_cat(self) -> dict:
+        return self._current_cat
 
-
-# This updates the current pool of images in static/images from the API
-
-scheduler.start()
-    
-    
+# Schedule all tasks:
+cur_ph = CurrentPhotos()
+schedule.every().hour.do(cur_ph.update_cur_image())
+# Get 24 random cat images at midnight
+schedule.every().day.at('23:55').do(cur_ph.update_images_dir())
+ 
     
 @app.route("/") 
 def home(): 
     
+    cat = cur_ph.get_curr_cat()
+    cat_description = 
     
-    cat_description = 'cat cat cat'
     return render_template('home.html',  
-                           cat_foto=CURRENT_IMAGE) 
+                           cat_foto=cat['url']) 
   
+def test():
+    # update_images_dir()
+    pass
+    
+    
 # run the application 
 if __name__ == "__main__": 
-    app.run(debug=True)
+    # app.run(debug=True)
+    test()
